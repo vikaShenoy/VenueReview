e451a01b7a918e3bf6263f4de410985b7b3d6220
Vikas Shenoy <vsh33@uclive.ac.nz>
2019-05-12 19:16:32 +1200

    Initial commit.

---
+++ src/Signup.vue
@@ -0,0 +1,75 @@
+<script>
+  export default {
+    data() {
+      return {
+        firstName: "",
+        lastName: "",
+        username: "",
+        email: "",
+        password: ""
+      }
+    },
+    mounted: function() {
+      console.log("Mounting complete.");
+    },
+    methods: {}
+  }
+</script>
+
+<template>
+
+  <div>
+    <v-app>
+      <v-container>
+
+        <v-card id="signup" class="card">
+          <h2 class="headings">Sign Up</h2>
+        </v-card>
+
+        <v-card>
+          <v-text-field
+            v-model="firstName"
+            label="First Name"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="lastName"
+            label="Last Name"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="username"
+            label="Username"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="email"
+            label="Email"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            label="Password"
+          ></v-text-field>
+        </v-card>
+
+      </v-container>
+    </v-app>
+  </div>
+
+</template>
+
+<style lang="css" scoped>
+  #signup {
+    align-self: center;
+    padding: 20px;
+    background: powderblue;
+  }
+  .headings {
+    color:black;
+    font-family: 'Roboto', sans-serif;
+  }
+
+</style>
+
+
