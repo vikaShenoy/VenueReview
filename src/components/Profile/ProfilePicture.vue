f12972ed1c18be4053630e7fa87095cc218989c8
vsh33 <vsh33@uclive.ac.nz>
2019-05-15 00:53:18 +1200

    Photo upload worked on. - Added file reader functionality to upload a photo. - Binary data is still slightly inaccurate.

--- src/components/Profile/ProfilePicture.vue
+++ src/components/Profile/ProfilePicture.vue
@@ -6,18 +6,66 @@
       return {
         selectedPhoto: null,
         ownProfile: false,
-        profilePhoto: null
+        profilePicData: "",
+        reader: null,
       }
     },
 
     methods: {
+
+
+      onFileChanged(event) {
+        let file = event.target.files[0];
+        this.reader = new FileReader();
+
+        // Closure to capture the file information.
+        this.reader.onload = (function(theFile) {
+          return function(e) {
+            // Render thumbnail.
+            console.log(e.target);
+          };
+        })(file);
+
+        // Read in file as binary data.
+        this.reader.readAsBinaryString(file);
+      },
+
+      onUpload() {
+        console.log(this.reader.result);
+        let data = this.reader.result;
+        const headers = {
+          'X-Authorization': localStorage.getItem("authToken"),
+          'Content-type': 'image/png'
+        };
+        this.$http.put(
+          "http://localhost:4941/api/v1/users/" + localStorage.getItem("userId") + "/photo",
+          data, {headers: headers})
+          .then(function (response) {
+            console.log(response);
+          }, function (error) {
+            console.log(error);
+          });
+      }
     }
   }
-
 </script>
 
 <template>
   <v-card class="uk-card uk-padding" style="margin: 10px">
+
+    <input type="file" @change="onFileChanged">
+
+    <v-btn
+      depressed
+      color="blue"
+      dark
+      @click="onUpload"
+    >Upload Photo</v-btn>
+
+    <br>
+    <br>
+    <br>
+    <output id="list"></output>
 
   </v-card>
 </template>
