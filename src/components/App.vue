01874ab668871c5c76a5b634dafd3f783a5d050f
vsh33 <vsh33@uclive.ac.nz>
2019-05-16 10:12:36 +1200

    Started restructuring project. - Moved main file to routers folder. - Started work on navbar component.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,21 @@
+<script>
+  import NavBar from '../components/Navbar.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      'Navigation': NavBar,
+    }
+  }
+</script>
+
 <template>
   <div id="app">
+    <img src="../assets/logo.png">
+    <router-view/>
+    <Navigation></Navigation>
     <router-view></router-view>
+
   </div>
 </template>
 
