4be5d9e419b4248ef9c9e847ed8d69fa6b8302d5
Vikas Shenoy <vsh33@uclive.ac.nz>
2019-05-18 13:21:46 +1200

    Continued working on venues component. - Refactored so that filtering is now done in a computed function. - Added filtering for catergories. - Setup/initialisation still needs to be cleaned up.

--- src/components/Venues.vue
+++ src/components/Venues.vue
@@ -17,29 +17,42 @@
           { text: 'Cost Rating (Mode)', value: 'modeCostRating' },
           { text: 'Photo', value: 'photoData' }
         ],
-        allVenues: [],
-        selectedVenues: [],
-        cities: [],
-        searchTerm: "",
+        unfilteredVenues: [],
+        filteredVenues: [],
+        cityList: [],
+        categoryList: [],
+        selectedSearchTerm: "",
+        selectedCity: "",
+        selectedCategory: ""
       }
     },
     mounted: function() {
-      console.log('Venues mounted');
-      this.getVenues();
-      this.selectedVenues = this.allVenues;
+      this.setup();
+    },
+    computed: {
+      filterTableData() {
+        console.log(this.selectedCity);
+        let filteredVenues = this.unfilteredVenues;
+
+        if (this.selectedSearchTerm) {
+          filteredVenues = filteredVenues.filter(
+            venue => venue.venueName.toLowerCase().includes(this.selectedSearchTerm.toLowerCase()));
+        }
+
+        if (this.selectedCity) {
+          filteredVenues = filteredVenues.filter(
+            venue => venue.city.toLowerCase() === this.selectedCity.toLowerCase());
+        }
+
+        if (this.selectedCategory) {
+          filteredVenues = filteredVenues.filter(
+            venue => venue.category.categoryName.toLowerCase() === this.selectedCategory.toLowerCase());
+        }
+        return filteredVenues;
+      }
     },
     methods: {
-      getCities(venueData) {
-        let cityList = [];
-        for (let i = 0 ; i < venueData.length ; i++) {
-          cityList.push(venueData[i].city);
-        }
-        // To enable the user to select all cities
-        cityList.push("All");
-        this.cities = cityList;
-      },
-      getVenues() {
-        console.log("Get venues called.");
+      setup() {
         let headers = {
           'X-Authorization': localStorage.getItem("authToken")
         };
@@ -47,67 +60,48 @@
           headers: headers
         })
           .then(function(response) {
-            this.allVenues = response.body;
-            this.selectedVenues = this.allVenues;
-            this.loadPhotos();
-            this.getCities(response.body);
-            this.loadPhotos();
+            this.getVenueDetails(response.body);
           }, function(error) {
             console.log(error);
           })
       },
-      loadPhotos() {
-        console.log("Load photos called");
-        console.log("Selected venues original");
-        console.log(this.selectedVenues);
-        for (let i = 0 ; i < this.selectedVenues.length ; i++) {
-          let venue = this.selectedVenues[i];
-          let headers = {
-            'X-Authorization': localStorage.getItem("authToken")
-          };
-          this.$http.get("http://localhost:4941/api/v1/venues/" + venue.venueId + "/photos/" + venue.primaryPhoto, {
-            headers: headers
-          })
-            .then(function(response) {
-              this.selectedVenues[i].photoData =  response.body;
-            }, function(error) {
-              console.log(error);
-            });
-        }
-        console.log("selected venues after");
-        console.log(this.selectedVenues);
-      },
-      filterCities(city) {
-        console.log(city === "All");
-        if (city !== "All") {
-          let venueList = [];
-          for (let i = 0; i < this.allVenues.length; i++) {
-            if (this.allVenues[i].city === city) {
-              venueList.push(this.allVenues[i]);
-            }
-          }
-          this.selectedVenues = venueList;
-        } else {
-          this.selectedVenues = this.allVenues;
-        }
-      },
-      searchVenues() {
-        console.log("Search term = " + this.searchTerm);
+
+      getVenueDetails(venues) {
         let headers = {
           'X-Authorization': localStorage.getItem("authToken")
         };
-        if (this.searchTerm.length !== 0) {
-          this.$http.get("http://localhost:4941/api/v1/venues?q=" + this.searchTerm, {
+        let venueList = [];
+        for (let i = 0 ; i < venues.length ; i++) {
+          this.$http.get("http://localhost:4941/api/v1/venues/" + venues[i].venueId, {
             headers: headers
           })
             .then(function(response) {
-              this.selectedVenues = response.body;
+              response.body.meanStarRating = venues[i].meanStarRating;
+              response.body.modeCostRating = venues[i].modeCostRating;
+              this.cityList.push(response.body.city);
+              this.categoryList.push(response.body.category.categoryName);
+              venueList.push(response.body);
             }, function(error) {
               console.log(error);
             })
-        } else {
-          this.selectedVenues = this.allVenues;
         }
+        this.unfilteredVenues = venueList;
+        //this.getCategories();
+      },
+
+      getCities() {
+        console.log(10);
+        let venues = this.unfilteredVenues;
+        console.log(this.unfilteredVenues);
+        this.cityList = venues.map(venue => venue.city);
+        this.cityList.unshift("");
+        console.log(this.cityList);
+      },
+
+      getCategories() {
+        let venues = this.unfilteredVenues;
+        this.categoryList = venues.map(venue => venue.category.categoryName);
+        this.categoryList.unshift("");
       }
     },
   }
@@ -120,32 +114,35 @@
         <v-layout row>
           <v-flex xs12 sm6 d-flex>
             <v-select
-              chips
               solo
-              :items="cities"
+              :items="cityList"
               label="City"
-              @change="filterCities($event)"
+              v-model="selectedCity"
+            ></v-select>
+            <v-select
+              solo
+              :items="categoryList"
+              label="Category"
+              v-model="selectedCategory"
             ></v-select>
             <v-text-field
-              v-model="searchTerm"
+              v-model="selectedSearchTerm"
               label="Search"
-              @blur="searchVenues()"
             ></v-text-field>
           </v-flex>
         </v-layout>
         <v-data-table
           :headers="headers"
-          :items="selectedVenues"
+          :items="filterTableData"
           class="elevation-1"
           dark
         >
           <template v-slot:items="props">
             <td>{{ props.item.venueName }}</td>
-            <td class="text-xs-right">{{ props.item.categoryId }}</td>
+            <td class="text-xs-right">{{ props.item.category.categoryName }}</td>
             <td class="text-xs-right">{{ props.item.city }}</td>
             <td class="text-xs-right">{{ props.item.meanStarRating }}</td>
             <td class="text-xs-right">{{ props.item.modeCostRating }}</td>
-<!--            <td class="text-xs-right">{{ props.item.photoData }}</td>-->
             <td>
               <img :src="props.item.photoData">
             </td>
